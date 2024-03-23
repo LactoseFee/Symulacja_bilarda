adb1bcce337da999283611837b6d476061425954
Rafał Nowakowski <rafael.now203@gmail.com>
2024-03-23 09:56:48 +0100

    stworzenie okna i dodanie menu

--- src/pl/edu/pw/fizyka/java/bilard/MainWindow.java
+++ src/pl/edu/pw/fizyka/java/bilard/MainWindow.java
@@ -1,9 +1,41 @@
 package pl.edu.pw.fizyka.java.bilard;
 
-public class MainWindow {
+import java.awt.HeadlessException;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+
+public class MainWindow extends JFrame {
+
+	JMenuBar menuBar = new JMenuBar();
+	JMenu menu = new JMenu("Menu");
+	JMenuItem item = new JMenuItem();
+	JSlider strokePowerRegulation = new JSlider();
+	JPanel bottomPanel = new JPanel();
+	
+	
+	MainWindow() throws HeadlessException {
+		this.setSize(640, 480);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		this.setJMenuBar(menuBar);
+		menuBar.add(menu);
+		menu.add(item);
+		
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println("24");
+		MainWindow frame = new MainWindow();
+		frame.setVisible(true);
+		
+		
+		
 
 	}
 
