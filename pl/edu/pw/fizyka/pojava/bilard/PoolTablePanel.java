0f951cede50ecdd6b8e2de5017d4d8322fcf3397
LactoseFee <magdalena.pazdziora5@gmail.com>
2024-03-24 01:32:38 +0100

    probowalam dodac grafike

--- pl/edu/pw/fizyka/pojava/bilard/PoolTablePanel.java
+++ pl/edu/pw/fizyka/pojava/bilard/PoolTablePanel.java
@@ -1,7 +1,20 @@
 package pl.edu.pw.fizyka.pojava.bilard;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class PoolTablePanel extends JPanel {
+    public static void main(String[] args) throws IOException {
+        JPanel panel = new JPanel();
+
+        BufferedImage table = ImageIO.read(new File("./bilardtable.png"));
+        JLabel label = new JLabel(new ImageIcon(table));
+        panel.add(label);
+    }
+
 
 }
